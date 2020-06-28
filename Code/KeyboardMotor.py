4a29f648b3c23d668bd86ba44bbf568df94bc51f
Simon <restrepo.simon@gmail.com>
2020-06-28 22:47:03 +1000

    Late night update: added index.html(web page code) and scripts to move motors

--- Code/KeyboardMotor.py
+++ Code/KeyboardMotor.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python3
 # import curses and GPIO
 import curses
 import RPi.GPIO as GPIO
 
 # set GPIO numbering mode and define output pins
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
+# set pin 18 as pwm with a 100Hz frequency
+pwm = GPIO.PWM(12, 100)
 
 # Get the curses window, turn off echoing of keyboard to screen, turn on
 # instant (no waiting) key response, and use special values for cursor keys
@@ -22,31 +26,43 @@ try:
         char = screen.getch()
         if char == ord('q'):
             break
+        #The movement is defined here. For a better understanting check the Hardware section on the GitHub page
         elif char == curses.KEY_UP:
-            GPIO.output(7, False)
-            GPIO.output(11, True)
-            GPIO.output(13, False)
-            GPIO.output(15, True)
+            GPIO.output(11, False)
+            GPIO.output(13, True)
+            GPIO.output(15, False)
+            GPIO.output(16, True)
         elif char == curses.KEY_DOWN:
-            GPIO.output(7, True)
-            GPIO.output(11, False)
-            GPIO.output(13, True)
-            GPIO.output(15, False)
-        elif char == curses.KEY_RIGHT:
-            GPIO.output(7, True)
-            GPIO.output(11, False)
+            GPIO.output(11, True)
             GPIO.output(13, False)
             GPIO.output(15, True)
-        elif char == curses.KEY_LEFT:
-            GPIO.output(7, False)
+            GPIO.output(16, False)
+        elif char == curses.KEY_RIGHT:
             GPIO.output(11, True)
-            GPIO.output(13, True)
+            GPIO.output(13, False)
             GPIO.output(15, False)
+            GPIO.output(16, True)
+        elif char == curses.KEY_LEFT:
+            GPIO.output(11, False)
+            GPIO.output(13, True)
+            GPIO.output(15, True)
+            GPIO.output(16, False)
         elif char == 10:
-            GPIO.output(7, False)
+            GPIO.output(16, False)
             GPIO.output(11, False)
             GPIO.output(13, False)
             GPIO.output(15, False)
+        #Defining the diferent speeds
+        elif char == 1:
+            pwm.ChangeDutyCycle(10)
+        elif char == 2:
+            pwm.ChangeDutyCycle(25)
+        elif char == 3:
+            pwm.ChangeDutyCycle(50)
+        elif char == 4:
+            pwm.ChangeDutyCycle(75)
+        elif char == 5:
+            pwm.ChangeDutyCycle(100)
 
 finally:
     # Close down curses properly, inc turn echo back on!
